e1739eae6a4077ddb2e935403895a30da2369461
Diego Torres <diegotorres0303@gmail.com>
2022-08-16 22:39:41 -0500

    final touches

--- build.ps1
+++ build.ps1
@@ -2,3 +2,4 @@ git commit -am "home"
 node .\scripts\readme2json.js
 node .\scripts\delete
 node .\scripts\run
+git commit -am "done"
--- install.ps1
+++ install.ps1
@@ -2,5 +2,6 @@ Write-Output "installing dependencien on root folder"
 npm install
 
 Set-Location infraestructure
+Write-Output "installing dependencien on infraestructure folder"
 npm install
 Set-Location ..
